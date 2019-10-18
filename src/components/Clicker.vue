5140572c30892feb01cf86b21f1be9be4bbd6e3d
kruglyyap <artem.kruglyy@oxagile.com>
2019-10-18 13:07:04 +0300

    init

---
+++ src/components/Clicker.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Clicker"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Custom.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Custom"
+    }
+</script>
+
+<style scoped>
+
+</style>
